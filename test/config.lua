32a38dc4129a6fbb018a8c410261587d56b5ec07
Denis Linnik <d.linnik@corp.mail.ru>
2017-03-28 20:05:26 +0300

    secret

--- test/config.lua
+++ test/config.lua
@@ -11,17 +11,17 @@ return {
 
     facebook = {
         client_id = '1813230128941062',
-        client_secret = '3bb5bbe8b72ff05bcf66ce9d5cbff3b3',
+        client_secret = 'some secret here',
         redirect_uri='http://localhost:8000/',
     },
     google = {
         client_id = '495340653331-3gmtvon6tc1o61ajn5piek6jgi0p2o47.apps.googleusercontent.com',
-        client_secret = 'aaFDCa0mMK8YqeBWOeAnfGYY',
+        client_secret = 'some secret here',
         redirect_uri='http://localhost:8000/',
     },
     vk = {
         client_id = '5873775',
-        client_secret = 'nwUFQsavhwIDR6ToDtX6',
+        client_secret = 'some secret here',
         redirect_uri='http://localhost:8000/',
     },
 }
